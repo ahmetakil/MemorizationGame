dce2cbae57d6525aceda891b6da09629cc5468f5
Ahmet Akıl <ahmetakil@fennelmarkets.com>
2023-11-12 12:53:36 +0300

    Initial Commit

---
+++ MemorizationGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemorizationGame
+//
+//  Created by Ahmet Akıl on 12.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemorizationGame/MemorizationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizationGameApp.swift
+//  MemorizationGame
+//
+//  Created by Ahmet Akıl on 12.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
